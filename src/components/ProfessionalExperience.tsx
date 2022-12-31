bee38e73388363778f2eb14553dc6bf5245077cf
Matthew Claysen <mclaysen@outlook.com>
2022-12-31 13:53:13 -0500

    Update styles

--- src/components/ProfessionalExperience.tsx
+++ src/components/ProfessionalExperience.tsx
@@ -2,15 +2,22 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
 import { ResumeMainProps, Role } from './ResumeProps';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import {theme} from '../theme/typeography';
+
 
 const ProfessionalExperience = ({role} : {role: Role}) => {
 
   return (<Box sx={{
     display: "flex"
   }}>
-  <Box><h1>{role.company}</h1></Box>
-  <Box><h1>{role.location.city}</h1></Box>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Box><Typography variant="h1">{role.company}</Typography></Box>
+    <Box><Typography variant="h5">{role.location.city}</Typography></Box>
+  </ThemeProvider>
   </Box>) 
 };
 
