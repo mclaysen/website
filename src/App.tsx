884b663b23b357b5919369fa43d5adedba8aebe8
Matthew Claysen <mclaysen@outlook.com>
2023-01-02 18:14:40 -0500

    Update react packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
 import Resume from './components/Resume';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
