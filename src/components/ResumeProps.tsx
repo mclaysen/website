c03ab1d112c9c91772b904c61757e8361c64b8f0
Matthew Claysen <mclaysen@outlook.com>
2023-02-19 21:57:52 -0500

    Adding additional information for the resume

--- src/components/ResumeProps.tsx
+++ src/components/ResumeProps.tsx
@@ -1,30 +1,58 @@
- interface Name {
-    first: string;
-    middleInitial: string;
-    last: string;
-  }
+interface Name {
+  first: string;
+  middleInitial: string;
+  last: string;
+}
   
 interface Address {
-   city: string;
-   state: string;
-   zip: string;
+  city: string;
+  state: string;
+  zip: string;
 }
 
 export interface Role {
-    company: string;
-    location: Address;
-    startDate: Date;
-    endDate?: Date;
-    currentRole: boolean;
-    title: string;
-    summary: string;
-    highlights: string[];
+  company: string;
+  location: Address;
+  startDate: Date;
+  endDate?: Date;
+  currentRole: boolean;
+  title: string;
+  summary: string;
+  highlights: string[];
 }
 
-export interface ResumeMainProps   {
-      name: Name;
-      address: Address;
-      phone: string;
-      email: string;
-      jobExperience?: Role[];
-  }
+export interface ResumeMainProps {
+  name: Name;
+  address: Address;
+  phone: string;
+  email: string;
+  jobExperience?: Role[];
+  education?: Education[];
+  otherExperiences?: OtherExperience[];
+  skills?: RelevantSkills[];
+}
+
+export interface OtherExperience {
+  category: string;
+  type: ExperienceInformation[];
+}
+
+export interface ExperienceInformation {
+  title: string;
+  subTitle?: string;
+  startDate: Date;
+  endDate?: Date;
+  highlights: string[];
+}
+
+export interface Education {
+  school: string;
+  location: Address;
+  degree: string;
+  dateObtained: Date;
+}
+
+export interface RelevantSkills {
+  category: string;
+  examples: string[];
+}
