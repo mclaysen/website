bc84491ff55d60fbd4beb1266fea4fdb8c3ae318
Matt Claysen <mclaysen@gmail.com>
2023-06-03 14:05:11 -0400

    create styled components

--- src/components/Resume.styles.tsx
+++ src/components/Resume.styles.tsx
@@ -1,3 +1,24 @@
+import styled from '@emotion/styled';
+import Typography from '@mui/material/Typography';
+import { theme } from '../theme/typeography';
+
 export const NameDiv = {
     align: "left"
 }
+
+export const Title = styled(Typography)(({ }) => ({
+    fontWeight: 'bold',
+    paddingBottom: '10px',
+    fontSize:`${theme.typography.body1.fontSize}`,
+    [theme.breakpoints.down('md')]: {
+        fontSize:`${theme.typography.h5.fontSize}`,
+    }
+}));
+
+export const DateInfo = styled(Typography)(({ }) => ({
+    display: 'inline',
+    fontSize:`${theme.typography.body1.fontSize}`,
+    [theme.breakpoints.down('md')]: {
+        fontSize:`${theme.typography.subtitle1.fontSize}`,
+    }
+}));
