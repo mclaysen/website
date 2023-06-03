691748661b3c8879954772086f4aadd7b5c8a7b1
Matt Claysen <mclaysen@gmail.com>
2023-06-03 14:27:18 -0400

    update education, other, and professional experience with new styled components

--- src/components/ProfessionalExperience.tsx
+++ src/components/ProfessionalExperience.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemText from '@mui/material/ListItemText'
-import Typography from '@mui/material/Typography';
+import Typography, { TypographyProps } from '@mui/material/Typography';
 import { Role } from './ResumeProps';
 import { ThemeProvider } from '@mui/material/styles';
 import {theme} from '../theme/typeography';
 import { GetDateInformation } from '../utils/GetDateInformation'
+import { useMediaQuery } from '@mui/material';
+import { DateInfo, Title } from './Resume.styles';
 
 const ProfessionalExperience = ({role, index} : {role: Role, index: Number}) => {
   role.startDate = new Date(role.startDate);
@@ -17,22 +18,27 @@ const ProfessionalExperience = ({role, index} : {role: Role, index: Number}) => 
     role.endDate = new Date(role.endDate);
   }
 
+  const dateContent = GetDateInformation(role.startDate, role.endDate, role.currentRole);
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+
   return (<Box sx={{
     display: "flex",
     textAlign: "left"
   }}>
     <ThemeProvider theme={theme}>
       <Grid container>
-        <Grid item xs={1}>
-          {index===0 ? <Box sx={{fontWeight: 'bold'}}>Experience</Box> : <Box></Box>}
+        <Grid item xs={12} md={2}>
+          {index===0 && <Title theme={theme}>Experience</Title>}
         </Grid>
-        <Grid item xs={9}>
+        {isSmallScreen && <Grid item xs={1}></Grid>}
+        <Grid item xs={11} md={8}>
           <Grid container spacing={0}>
             <Grid item xs={12}>
               <Box>
                 <Typography sx={{fontWeight: 'bold'}}>{role.company}</Typography>&nbsp;
                 <Typography>{role.location.city}</Typography>,&nbsp;
                 <Typography>{role.location.state}</Typography>
+                {isSmallScreen && <DateInfo>&nbsp;{dateContent}</DateInfo>}
               </Box>
             </Grid>
             <Grid item xs={12}>
@@ -48,15 +54,15 @@ const ProfessionalExperience = ({role, index} : {role: Role, index: Number}) => 
             <Grid item xs={12}>
               <List>
               {role.highlights?.map((value, index)=>{
-                    return <ListItem disablePadding><ListItemText inset primary = {value}></ListItemText></ListItem>
+                    return <ListItem key={index} disablePadding><ListItemText inset primary = {value}></ListItemText></ListItem>
                 })}
               </List>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={2}>
-          <Box sx={{textAlign: 'right'}}>{GetDateInformation(role.startDate, role.endDate, role.currentRole)}</Box>
-        </Grid>
+        {!isSmallScreen && <Grid item xs={2} sx={{textAlign: 'right'}}>
+          <DateInfo>{dateContent}</DateInfo>
+        </Grid>}
       </Grid>
     </ThemeProvider>
   </Box>) 
