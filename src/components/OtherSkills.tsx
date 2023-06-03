1108b0e256c54c0436d03423f5ae3a20520ec6e4
Matt Claysen <mclaysen@gmail.com>
2023-06-03 14:53:22 -0400

    update the resume props for other skills

--- src/components/OtherSkills.tsx
+++ src/components/OtherSkills.tsx
@@ -1,35 +1,42 @@
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { RelevantSkills } from './ResumeProps';
+import { SkillInformation } from './ResumeProps';
 import { ThemeProvider } from '@mui/material/styles';
 import {theme} from '../theme/typeography';
+import { Title } from './Resume.styles';
+import { useMediaQuery } from '@mui/material';
 
-const OtherSkills = ({skills} : {skills: RelevantSkills[]}) => {
-
+const OtherSkills = ({skills} : {skills: SkillInformation[]}) => {
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  
   return (<Box sx={{
     display: "flex",
     textAlign: "left"
   }}>
     <ThemeProvider theme={theme}>
-      <Grid container>
-        <Grid item xs={1}>
-          <Box sx={{fontWeight: 'bold'}}>Technology Skills</Box>
-        </Grid>
-        <Grid item xs={11}>
-        <Box>
-            {skills.map((value, index)=>{
-                return <Box>
-                    <Typography>{value.category}</Typography>&nbsp;
-                    ({value.examples.map((example, index)=>{ 
-                        let separator = index!==value.examples.length-1 ? ", " : "";
-                        return <Typography>{example}{separator}</Typography> 
-                    })}) 
-                </Box>
-            })}
+      {skills.map((skillType, index)=> {
+        return (
+        <Grid container>
+          <Grid item xs={12} md={2}>
+            <Title>{skillType.category}</Title>
+          </Grid>
+          {isSmallScreen && <Grid item xs={1}></Grid>}
+          <Grid item xs={11} md={8}>
+            <Box>
+              {skillType.specificSkills.map((skillInfo, index)=>{
+                  return <Box>
+                      <Typography sx={{fontWeight:'bold'}}>{skillInfo.type}</Typography>
+                      <br />
+                      <Box sx={{paddingLeft:'20px'}}>
+                        <Typography>{skillInfo.examples.join(", ")}</Typography>
+                      </Box>
+                  </Box>
+              })}
             </Box>
-        </Grid>
-      </Grid>
+          </Grid>
+        </Grid>)
+      })}
     </ThemeProvider>
   </Box>
   )
